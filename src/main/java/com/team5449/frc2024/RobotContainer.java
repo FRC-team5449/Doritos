4c3105e5b0ffc2020df5540b4413002eaac418e2
Chao-Freeman <124365937+Chao-Freeman@users.noreply.github.com>
2024-03-22 13:11:22 -0700

    Auto's update

--- src/main/java/com/team5449/frc2024/RobotContainer.java
+++ src/main/java/com/team5449/frc2024/RobotContainer.java
@@ -248,7 +248,7 @@ public class RobotContainer {
     NamedCommands.registerCommand("Intake", new WaitCommand(new IntakeCommand(shooter, intake), Double.POSITIVE_INFINITY, noteStored::get).alongWith(new InstantCommand(() -> armPoseCommand.setPose(ArmSystemState.INTAKE))));
     NamedCommands.registerCommand("NearShoot", new WaitCommand(new ShootCommand(shooter, () -> armPoseCommand.getArmState() == ArmSystemState.AUTOSHOOT, 65), 2).alongWith(new InstantCommand(() -> armPoseCommand.setAutoShootPosition(0.25))));
     NamedCommands.registerCommand("MiddleShoot", new WaitCommand(new ShootCommand(shooter, () -> armPoseCommand.getArmState() == ArmSystemState.AUTOSHOOT, 75), 2).alongWith(new InstantCommand(() -> armPoseCommand.setAutoShootPosition(0.22))));
-    NamedCommands.registerCommand("FarShoot", new WaitCommand(new ShootCommand(shooter, () -> armPoseCommand.getArmState() == ArmSystemState.AUTOSHOOT, 75), 3).alongWith(new InstantCommand(() -> armPoseCommand.setAutoShootPosition(0.192))));
+    NamedCommands.registerCommand("FarShoot", new WaitCommand(new ShootCommand(shooter, () -> armPoseCommand.getArmState() == ArmSystemState.AUTOSHOOT, 75), 3).alongWith(new InstantCommand(() -> armPoseCommand.setAutoShootPosition(0.1958))));
     NamedCommands.registerCommand("Arm Down", new InstantCommand(() -> armPoseCommand.setPose(ArmSystemState.ARMDOWN)));
   }
 
