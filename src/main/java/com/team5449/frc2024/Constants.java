9739cb8c5e3be5c1757d5b7915d2968b9c645b51
Chao-Freeman <124365937+Chao-Freeman@users.noreply.github.com>
2024-04-10 18:03:05 +0800

    @tanoom 's update
    Co-Authored-By: tanoom <68962425+tanoom@users.noreply.github.com>

--- src/main/java/com/team5449/frc2024/Constants.java
+++ src/main/java/com/team5449/frc2024/Constants.java
@@ -24,14 +24,14 @@ public class Constants {
     public static final double trackBaseMeters = 0.54;
     public static final double driveBaseRadius = Math.hypot(wheelBaseMeters / 2, trackBaseMeters / 2);
 
-    public static final double maxVelocityMeterPerSec = 7;
+    public static final double maxVelocityMeterPerSec = 8;
     public static final double maxAccelerationMeterPerSecSq = 100;
 
 
     public static final double maxAutoVelocityMeterPerSec = 2;
     public static final double maxAutoAccelerationMeterPerSecSq = 100;
 
-    public static final double maxAngularVelocityRadPerSec = 6; //maxVelocityMeterPerSec / Math.hypot(wheelBaseMeters / 2, trackBaseMeters / 2);
+    public static final double maxAngularVelocityRadPerSec = 10; //maxVelocityMeterPerSec / Math.hypot(wheelBaseMeters / 2, trackBaseMeters / 2);
 
     public static final int driveCurrentLimit = 70;
     public static final int steerCurrentLimit = 30;
