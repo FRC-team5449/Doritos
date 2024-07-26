f68592ec0f9cbc5304d53fd3a75f0e1cad7ac5d8
Chao-Freeman <124365937+Chao-Freeman@users.noreply.github.com>
2024-07-26 11:25:52 +0800

    test

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
-start /min ssh -C -J prototype robot "cd /usr/local/frc/bin; frcKillRobot.sh"
-@scp -C -J prototype build\libs\Doritos.jar robot:Doritos.jar
-ssh -C -J prototype robot "cd /usr/local/frc/bin; frcRunRobot.sh"
+@scp -C build\libs\Doritos.jar robot:Doritos.jar
+ssh -C robot "cd /usr/local/frc/bin; ./frcKillRobot.sh"
+::ssh -C -J prototype robot "cd /usr/local/frc/bin; frcRunRobot.sh"
 ::ssh -C -J prototype robot  "./robotCommand"
 pause
